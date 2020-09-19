fbc50e28841a577ce3c322ae6b7cb7e9732ba347
Magdalena Checinski <madzix.p730@gmail.com>
2020-09-19 21:06:31 +0200

    Created slider

---
+++ javaScript/slider.js
@@ -0,0 +1,13 @@
+
+const slideList = ['/images/slider1.jpg', '/images/slider2.jpg', '/images/slider3.jpg'] 
+const slider = document.querySelector('header')
+let active = 0
+const changeSlide = ()=>{
+  active++
+  if(active === slideList.length){
+    active = 0
+  }
+slider.style.backgroundImage = `url(${slideList[active]})`
+}
+setInterval(changeSlide, 7000)
+
