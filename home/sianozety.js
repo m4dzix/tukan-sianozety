aaf33ef947ea608eebfdc07405330b5b20c296d9
Magdalena Checinski <madzix.p730@gmail.com>
2020-09-19 22:24:50 +0200

    Clean up the code

--- home/sianozety.js
+++ home/sianozety.js
@@ -1,17 +1,28 @@
 
-initMap = () => {
-  const location = {
-    lat: 54.209220,
-    lng: 15.733520,
-  }
-  const Map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: location,
-  })
-}
-const xSign = document.querySelector(".close")
-const accept = document.querySelector(".accept")
+const xSign = document.querySelector(".js-close")
+const accept = document.querySelector(".js-accept")
 const cookie = document.querySelector(".cookie")
-const rodo = document.querySelector('.rodo')
+const rodo = document.querySelector(".rodo")
 xSign.addEventListener('click', ()=>{cookie.classList.add('hover')})
 accept.addEventListener('click', ()=>{rodo.classList.add('hover')})
+const burger = document.querySelector('.burger .fas')
+const nav = document.querySelector('nav')
+const body = document.querySelector('body')
+
+
+
+document.querySelector('.fas').addEventListener('click', () => {
+  document.querySelector('.popup').classList.add('close')
+  cookie.classList.remove('hover')
+  rodo.classList.remove('hover')
+})
+
+xSign.addEventListener('click', () => {
+  cookie.classList.add('close')
+})
+accept.addEventListener('click', () => {
+  rodo.classList.add('hover'), cookie.style.bottom = 2 + '%'
+})
+burger.addEventListener('click', () => {
+  nav.classList.toggle('show')
+})
