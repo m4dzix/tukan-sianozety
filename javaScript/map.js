a56d46aead20f9228ca55ef5989e89b2edb3f6c1
Madlen <madlen@SoftRay.local>
2021-01-12 14:37:38 +0100

    Error handling for map

--- javaScript/map.js
+++ javaScript/map.js
@@ -1,10 +1,16 @@
 initMap = () => {
-  const location = {
-    lat: 54.20922,
-    lng: 15.73352,
-  };
-  const Map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: location,
-  });
+  try {
+    const location = {
+      lat: 54.20922,
+      lng: 15.73352,
+    };
+    const Map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: location,
+    });
+  } catch (error) {
+    console.error(error);
+    const Map = document.getElementById("map");
+    Map.innerHTML += "Nie udało się pobrać mapy, przepraszamy";
+  }
 };
