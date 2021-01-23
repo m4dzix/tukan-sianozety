d6c686c7303e23cfeaa589acaec24ebd262457c3
Madlen <madlen@SoftRay.local>
2021-01-23 15:56:13 +0100

    Change of file paths in slider to index.html

--- javaScript/slider.js
+++ javaScript/slider.js
@@ -3,13 +3,20 @@ const slideList = [
   "/images/slider2.jpg",
   "/images/slider3.jpg",
 ];
+const homeSlideList = [
+  "images/slider1.jpg",
+  "images/slider2.jpg",
+  "images/slider3.jpg",
+];
 const slider = document.querySelector("header");
+const homeSlider = document.querySelector(".home");
 let active = 0;
 const changeSlide = () => {
   active++;
-  if (active === slideList.length) {
+  if (active === homeSlideList.length) {
     active = 0;
   }
   slider.style.backgroundImage = `url(${slideList[active]})`;
+  homeSlider.style.backgroundImage = `url(${homeSlideList[active]})`;
 };
 setInterval(changeSlide, 7000);
